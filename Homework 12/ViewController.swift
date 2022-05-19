39e71685acdd33f9982e828643daa2794888e7ef
DornPorn <gungan722121@gmail.com>
2022-05-19 12:17:09 +0700

    Initial Commit

---
+++ Homework 12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 12
+//
+//  Created by Stanislav Rassolenko on 5/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
